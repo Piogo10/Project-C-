cc8efaacf2b434005db96aca7f51544663db7d10
Gugas749 <conradoalexandre00@gmail.com>
2024-11-15 12:13:28 +0000

    first commit

---
+++ projeto.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#pragma VARIAVEIS
+
+//STUDENT INFO
+int studentID = 0, studentNumber;
+char studentName, studentEmail;
+
+//FICHA INFO
+int fichaID = 0, fichaExNum;
+char fichaName, fichaDate;
+
+//EXERCICIO INFO
+int exercicioID = 0;
+int parentFichaID;
+int exercicioDifficulty; // 1 a 3
+int exercicioType; // 1 a 2
+char exercicioName;
+
+#pragma endregion
+
+
+void main(){
+    
+}
+
+void getStudentInfo(){
+    
+}
+
+void getFichaInfo(){
+
+}
